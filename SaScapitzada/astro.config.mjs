e82493a25435410be71c18d1d666cf4655001f6a
Samuses <75915561+Samuses@users.noreply.github.com>
2026-02-04 18:42:38 +0100

    Init
    i did the inini

--- SaScapitzada/astro.config.mjs
+++ SaScapitzada/astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx'; // astro guides integrations
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+    site: 'https://samuses.github.io', // url site (comunica dove è il sito senza lo /)
+    base: '/SaScapitzada', // base path for the site (percorso base del sito)
+    trailingSlash: "ignore", // assicurati che l'URL del sito usi lo "/" finale 
+
+    redirects:{
+        '/':'./it/', // reindirizza alla pagina in italiano
+    },
+
+    integrations:[
+        // aggiunge eventuali integrazioni tipo: file mdx,react ecc...
+        mdx(),
+    ]
+
+});
